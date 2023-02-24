04cad7c4e8ae1939e2658075ab595341f0833dcc
Piergiorgio Di Pasquale <66187691+joe-pier@users.noreply.github.com>
2023-02-24 19:40:45 +0100

    fix security flaw

--- database.py
+++ database.py
@@ -2,7 +2,16 @@
 from sqlalchemy import create_engine, text
 import os
 db_connection = os.environ["DATABASE_CREDENTIALS"] 
-print(db_connection)
+
+# how to set a secret variable:
+# - set the secret variable in github secrets (settings > secrets and variables > actions > repository secrets). 
+#   this can be called in the workflow via ${{  secrets.DATABASE_CREDENTIALS  }}
+# - at this point we need to set the variable as a enviroment variable everytime render deploys.
+#   to do this we create a workflow that sets the enviroment variable DATABASE_CREDENTIALS as ${{ secrets.DATABASE_CREDENTIALS }}
+# - now each time we deploy the workflow triggers and the secret variable  secrets.DATABASE_CREDENTIALS is set as an enviroment variable and 
+#   we can get it in python via db_connection = os.environ["DATABASE_CREDENTIALS"] 
+
+
 engine = create_engine(db_connection, connect_args= 
     {
     "ssl": {
