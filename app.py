c6206bb0e87559b8471b87173079f130fb778b23
Piergiorgio Di Pasquale <66187691+joe-pier@users.noreply.github.com>
2023-02-24 19:30:53 +0100

    encryption on

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, jsonify,request,send_file
+from database import load_jobs_from_db
+app = Flask(__name__) # instance of class Flask
+
+UPLOAD_FOLDER = './uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route("/") # any website has a route. a part of the url after the url
+# this is going to match the empty route
+def home():
+    jobs = load_jobs_from_db()
+    return render_template('home.html', jobs= jobs, name = "Pier")
+
+@app.route("/api/jobs")
+def list_jobs():
+    jobs = load_jobs_from_db()
+    return jsonify(jobs)
+
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", debug = True)
