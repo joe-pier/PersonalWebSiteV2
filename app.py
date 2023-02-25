d83b9fa5945b2fe9f8506dd619e23acbf7c0e85e
Piergiorgio Di Pasquale <66187691+joe-pier@users.noreply.github.com>
2023-02-25 18:59:10 +0100

    added form aesthetics

--- app.py
+++ app.py
@@ -1,33 +1,43 @@
 from flask import Flask, render_template, jsonify
 from database import load_jobs_from_db, load_job_from_db
 
-app = Flask(__name__) # instance of class Flask
+app = Flask(__name__)  # instance of class Flask
 
 UPLOAD_FOLDER = './uploads'
 
-@app.route("/") # any website has a route. a part of the url after the url
+
+@app.route("/")  # any website has a route. a part of the url after the url
 # this is going to match the empty route
 def home():
     jobs = load_jobs_from_db()
-    return render_template('home.html', jobs= jobs ,name = "Pier")
+    return render_template('home.html', jobs=jobs, name="Pier")
+
 
 @app.route("/api/jobs")
 def list_jobs():
     jobs = load_jobs_from_db()
     return jsonify(jobs)
 
+
 @app.route("/api/job/<id>")
 def show_job_api(id):
     job = load_job_from_db(id)
     return jsonify(job)
 
+
 @app.route("/job/<id>")
 def show_job(id):
     job = load_job_from_db(id)
     if not job:
-        return render_template("404.html", job = job) 
+        return render_template("404.html", job=job)
     else:
-        return render_template("jobpage.html", job = job)
+        return render_template("jobpage.html", job=job)
+
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", debug = True)
+    app.run(host="0.0.0.0", debug=True)
