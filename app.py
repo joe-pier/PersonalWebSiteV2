d1cc622e08a45b9fea5868356385b320ba01d210
Piergiorgio Di Pasquale <66187691+joe-pier@users.noreply.github.com>
2023-02-26 17:31:19 +0100

    fixes

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ app.config['XCAPTCHA_DIV_CLASS'] = "h-captcha"
 xcaptcha = XCaptcha(app=app)
 
 
-@app.route("/")  # any website has a route. a part of the url after the url
-# this is going to match the empty route
+@app.route("/")  # any website has a route. a part of the url after the url this is going to match the empty route
+
 def home():
     jobs = load_jobs_from_db()
     return render_template('home.html', jobs=jobs, name="Pier")
@@ -80,7 +80,7 @@ def login():
 
 @app.route("/login/data", methods=["post"])
 def login_data():
-    cv = True# xcaptcha.verify()
+    cv = xcaptcha.verify()
     if cv:
         login_data_query = get_login_info()
         login_data = request.form
